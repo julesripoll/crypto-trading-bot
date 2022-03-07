32569d6ecc54c118840ad7b3b399d5ca43565b36
Jules RIPOLL <jules.ripoll@gmail.com>
2022-03-07 11:35:54 +0100

    test2

---
+++ config_alpaca.py
@@ -0,0 +1,8 @@
+API_KEY="PKM93ZND8DXBQNVAXDUU" #obtenu à l'adresse suivante : https://app.alpaca.markets/paper/dashboard/overview permet de requêter l'api d'alpaca
+SECRET_KEY="qPsefXwMsB6OLJNu0zZ5Q4FtLByNMl7LDnqgYna3"
+HEADERS={
+    "APCA-API-KEY-ID":API_KEY,
+    "APCA-API-SECRET-KEY":SECRET_KEY
+}
+BARS_URL="https://data.alpaca.markets/v2/bars"
+BASE_URL="https://paper-api.alpaca.markets"
