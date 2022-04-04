fdb935642a2852292cc26f504819e5e83bd47cd1
Benjamin Drai <benjamin.drai@gmail.com>
2022-04-04 11:58:06 +0200

    Tests Docker for extract_data

--- Benjamin/run_export.sh
+++ Benjamin/run_export.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t extract_data .
-docker run --rm --network="host" -v $(pwd)/CSV_Files/:/app/CSV_Files extract_data
+docker run --rm -v ${PWD}/csv_files/:/app/csv_files extract_data
