815b903433e735fb45ba5fe7dc34a774b26214ed
Jules Ripoll <83707644+julesripoll@users.noreply.github.com>
2022-06-02 21:20:49 +0200

    ajout de lignes d'eval pour l'agent

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 """main program. on va pouvoir l'appeller en ligne de commande avec plusieurs modes genre train, test local, run online etc"""
 import functions, params
 import matplotlib.pyplot as plt
+from tf_agents.metrics import tf_metrics
+from tf_agents.drivers.dynamic_episode_driver import DynamicEpisodeDriver
 
 train_env=params.train_env
 agent=params.agent
 
-losses=functions.train(environment=train_env,agent=agent,train_ite=10)
+losses=functions.train(environment=train_env,agent=agent,train_ite=2)
+
+avg_return=tf_metrics.AverageReturnMetric()
+observer=[avg_return]
+eval_env=params.eval_env
+time_step = eval_env.reset()
+print(time_step)
+driver=DynamicEpisodeDriver(env=eval_env,policy=agent.policy,observers=observer,num_episodes=10)
+time_step, _ = driver.run(time_step)
+
 
 plt.plot(losses)
-plt.savefig('loss1.png')
+plt.savefig('loss.png')
 plt.show()
-
-
 
 
